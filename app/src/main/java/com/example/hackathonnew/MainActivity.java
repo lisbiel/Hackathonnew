50e63d31801239626ffe69c9b111566ecff15e57
Leonardo Vedovate <lvedovate@outlook.com>
2019-05-04 13:33:18 -0300

    wrtetr

--- app/src/main/java/com/example/hackathonnew/MainActivity.java
+++ app/src/main/java/com/example/hackathonnew/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.hackathonnew;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button souGuia = findViewById(R.id.sou_guia);
+
+        souGuia.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, NovaTela.class);
+                startActivity(it);
+            }
+        });
+        Button estouperdido = findViewById(R.id.estou_perdido);
+
+        souGuia.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, NovaTela.class);
+                startActivity(it);
+            }
+        });
     }
 }
