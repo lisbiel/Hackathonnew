39758d1c342144de68f48d826786b502f4639275
Leonardo Vedovate <lvedovate@outlook.com>
2019-05-04 14:54:23 -0300

    wrtetr

--- app/src/main/java/com/example/hackathonnew/PerdidaActivity.java
+++ app/src/main/java/com/example/hackathonnew/PerdidaActivity.java
@@ -1,14 +1,25 @@
 package com.example.hackathonnew;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class PerdidaActivity extends AppCompatActivity {
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.cadastro_pessoa_perdida);
 
+        Button encontrarguia = findViewById(R.id.estou_perdido);
+
+        encontrarguia.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(PerdidaActivity.this, PerdidaActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
